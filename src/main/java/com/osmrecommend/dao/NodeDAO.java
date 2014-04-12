f0a4506132a095af803f031f3495c2b8afde5441
Vivek <vr3690@gmail.com>
2014-04-12 16:08:44 -0500

    Updated schema to pgsnapshot.

--- src/main/java/com/osmrecommend/dao/NodeDAO.java
+++ src/main/java/com/osmrecommend/dao/NodeDAO.java
@@ -1,17 +1,12 @@
 package com.osmrecommend.dao;
 
 import it.unimi.dsi.fastutil.longs.LongSet;
-import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 
 import java.util.HashSet;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.grouplens.lenskit.data.dao.ItemDAO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +15,6 @@ import com.osmrecommend.persistence.service.NodeService;
 @Component
 public class NodeDAO implements ItemDAO {
 
-	private static Logger log = LoggerFactory.getLogger(NodeDAO.class);
-	
 	@Autowired
 	NodeService service;
 	
@@ -31,51 +24,11 @@ public class NodeDAO implements ItemDAO {
 	}
 	
 	public ObjectList<String> getItemTags(long item) {
-		
-		ObjectList<String> itemTags = new ObjectArrayList<String>();
-		
-		Object2ObjectMap<String, String> mapOfItemTags = service.getTagsForNodeId(item);
-		
-		for(Entry<String, String> entry : mapOfItemTags.entrySet()) {
-		
-			itemTags.add(entry.getKey()+entry.getValue());
-			
-		}
-		
-		return itemTags;
+		return service.getTagsForNodeId(item);
 	}
 	
 	public Set<String> getTagVocabulary() {
-		
-		Set<String> tagVocabulary = new HashSet<String>();
-		
-		Object2ObjectMap<String, String> mapOfItemTags = null;
-		
-		if(null == service) {
-			log.info("service is null.");
-		} else {
-			log.info("service isn't null.");
-		}
-		
-		if(null == (mapOfItemTags = service.getAllTags())) {
-			log.info("allags is null");
-		} else {
-			log.info("alltags isn't null.");
-		}
-				
-		for(Entry<String, String> entry : mapOfItemTags.entrySet()) {
-			
-			tagVocabulary.add(entry.getKey()+entry.getValue());
-			
-		}
-		
-		return tagVocabulary;
+		return new HashSet<String>(service.getAllTags());
 	}
-	
-	/*public NodeDAO() {
-		
-		service = new NodePersistenceServiceImpl();
-		
-	}*/
 
 }
