b2ce25aa62a036cc512a951de72b68cccf7fb362
Vivek <vr3690@gmail.com>
2014-04-12 16:04:46 -0500

    Code cleanup.

--- src/main/java/com/osmrecommend/app/OSMRecommend.java
+++ src/main/java/com/osmrecommend/app/OSMRecommend.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 import com.osmrecommend.cbf.TFIDFItemScorer;
 import com.osmrecommend.config.JPAConfiguration;
 import com.osmrecommend.dao.NodeDAO;
-import com.osmrecommend.dao.OSMRecommendUserDAO;
+import com.osmrecommend.dao.CustomUserDAO;
 import com.osmrecommend.data.event.dao.EditDAO;
 import com.osmrecommend.data.event.edit.NodeEdit;
 
@@ -36,7 +36,6 @@ public class OSMRecommend {
 		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
 		appContext.register(JPAConfiguration.class);
 		appContext.refresh();
-		//JPAConfiguration jpaConfig = appContext.getBean(JPAConfiguration.class);
 		
 		/*Lenkskit*/
 		
@@ -51,7 +50,7 @@ public class OSMRecommend {
 		
 		lenskitConfig.bind(Event.class).to(NodeEdit.class);
 		
-		lenskitConfig.bind(UserDAO.class).to(OSMRecommendUserDAO.class);
+		lenskitConfig.bind(UserDAO.class).to(CustomUserDAO.class);
 		
 		lenskitConfig.bind(ItemDAO.class).to(appContext.getBean(NodeDAO.class));
 
@@ -66,41 +65,8 @@ public class OSMRecommend {
 				log.info(scoredId.getId() +" - "+scoredId.getScore());
 			}
 		} catch (RecommenderBuildException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// Create the recommender
-		/*log.info("emf...");
-		EntityManager manager;
-		try {
-			manager = jpaConfig.entityManager(jpaConfig.entityManagerFactory());
-			SessionImpl session = (SessionImpl) manager.unwrap(Session.class);
-			session.doWork(new Work() {
-
-				@Override
-				public void execute(Connection connection) throws SQLException {
-					try {
-						log.info("creating recommender");
-						LenskitRecommender rec = LenskitRecommender.build(lenskitConfig);
-						ItemRecommender irec = rec.getItemRecommender();
-						log.info("fetching recommendations");
-						List<ScoredId> recommendations = irec.recommend(1538030, 5);
-						log.info("recos fetched");
-						for(ScoredId scoredId : recommendations) {
-							System.out.println(scoredId.getId() +" - "+scoredId.getScore());
-						}
-					} catch (RecommenderBuildException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			});
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}*/
-		
 	}	
 	
 }
