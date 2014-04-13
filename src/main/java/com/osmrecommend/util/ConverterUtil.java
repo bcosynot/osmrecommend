b5bc92af453eafb5fd3bd3b47a7ab425208da598
Vivek <vr3690@gmail.com>
2014-04-13 14:58:29 -0500

    Refactored helper method.

---
+++ src/main/java/com/osmrecommend/util/ConverterUtil.java
@@ -0,0 +1,27 @@
+package com.osmrecommend.util;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+
+import java.util.Map.Entry;
+
+public class ConverterUtil {
+
+	/**
+	 * @param mapOfTags
+	 */
+	public static ObjectList<String> convertMapOfTagsToCombinedList(Object2ObjectMap<String, String> mapOfTags) {
+		
+		ObjectList<String> tags = new ObjectArrayList<String>();
+		
+		for (Entry<String, String> e : mapOfTags.entrySet()) {
+
+			tags.add(e.getKey().toLowerCase() + e.getValue().toLowerCase());
+
+		}
+		
+		return tags;
+	}
+	
+}
