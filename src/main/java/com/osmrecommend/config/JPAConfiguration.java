8f104c87db959cded3abd29216b8acbb6f47a900
Vivek <vr3690@gmail.com>
2014-04-13 11:23:59 -0500

    Updated connection pooling settings.

--- src/main/java/com/osmrecommend/config/JPAConfiguration.java
+++ src/main/java/com/osmrecommend/config/JPAConfiguration.java
@@ -1,5 +1,6 @@
 package com.osmrecommend.config;
 
+import java.beans.PropertyVetoException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +14,6 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -21,6 +21,7 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.osmrecommend.dao.AreaDAO;
 import com.osmrecommend.dao.NodeDAO;
 import com.osmrecommend.dao.WayDAO;
@@ -32,15 +33,15 @@ import com.osmrecommend.data.event.dao.EditDAO;
 public class JPAConfiguration {
 	
 	@Bean
-	public DriverManagerDataSource dataSource() throws SQLException {
+	public ComboPooledDataSource dataSource() throws SQLException {
 		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
 		
 		return dataSource;
 	}
 	
 	@Bean
-	public EntityManagerFactory entityManagerFactory() throws SQLException {
+	public EntityManagerFactory entityManagerFactory() throws SQLException, PropertyVetoException {
 		
 		CompositeConfiguration config = new CompositeConfiguration();
 		try {
@@ -54,11 +55,18 @@ public class JPAConfiguration {
 		Map<String, Object> dbConnectionProperties = new HashMap<String, Object>();
 		dbConnectionProperties.put("hibernate.default_schema", dbConnectionConfig.getSchema());
 		dbConnectionProperties.put("hibernate.dialect", "org.hibernate.spatial.dialect.postgis.PostgisDialect");
+		dbConnectionProperties.put("hibernate.c3p0.acquire_increment", 1);
 		dbConnectionProperties.put("hibernate.c3p0.min_size", 5);
 		dbConnectionProperties.put("hibernate.c3p0.max_size", 20);
-		dbConnectionProperties.put("hibernate.c3p0.timeout", 300);
-		dbConnectionProperties.put("hibernate.c3p0.max_statements", 50);
-		dbConnectionProperties.put("hibernate.c3p0.idle_test_period", 3000);
+		dbConnectionProperties.put("hibernate.c3p0.timeout", 100);
+		dbConnectionProperties.put("hibernate.c3p0.max_statements", 30);
+		dbConnectionProperties.put("hibernate.c3p0.idle_test_period", 300);
+		dbConnectionProperties.put("acquireIncrement", 1);
+		dbConnectionProperties.put("minPoolSize", 5);
+		dbConnectionProperties.put("maxPoolSize", 3);
+		dbConnectionProperties.put("timeout", 100);
+		dbConnectionProperties.put("maxStatements", 30);
+		dbConnectionProperties.put("maxIdleTime", 300);
 		
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 	    vendorAdapter.setGenerateDdl(true);
@@ -67,10 +75,10 @@ public class JPAConfiguration {
 	    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 	    factory.setJpaVendorAdapter(vendorAdapter);
 	    factory.setPackagesToScan("com.osmrecommend.persistence");
-	    DriverManagerDataSource ds = dataSource();
-	    ds.setDriverClassName(dbConnectionConfig.getDriverClass());
-	    ds.setUsername(dbConnectionConfig.getUser());
-	    ds.setUrl(dbConnectionConfig.getConnectionURL());
+	    ComboPooledDataSource ds = dataSource();
+	    ds.setDriverClass(dbConnectionConfig.getDriverClass());
+	    ds.setUser(dbConnectionConfig.getUser());
+	    ds.setJdbcUrl(dbConnectionConfig.getConnectionURL());
 	    ds.setPassword(dbConnectionConfig.getPassword());
 	    factory.setDataSource(ds);
 	    factory.setJpaPropertyMap(dbConnectionProperties);
@@ -87,7 +95,7 @@ public class JPAConfiguration {
 	}
 
 	@Bean
-	public PlatformTransactionManager transactionManager() throws SQLException {
+	public PlatformTransactionManager transactionManager() throws SQLException, PropertyVetoException {
 
 		JpaTransactionManager txManager = new JpaTransactionManager();
 		txManager.setEntityManagerFactory(entityManagerFactory());
